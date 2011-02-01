d23e4743003a8d96fcd2474570f5b47038ccb857
I-Lin Kuo <ikuo@usgs.gov>
2011-02-01 17:38:22 +0000

    - committing DebugSettings used to control local debugging

--- src/main/java/gov/usgs/DebugSettings.java
+++ src/main/java/gov/usgs/DebugSettings.java
@@ -3,5 +3,8 @@ package gov.usgs;
 public class DebugSettings {
 	public static volatile boolean isTraceSet = false;
 
+	public static final String prodServerBase = "http://cida.usgs.gov";
+	public static final String localServerBase = "http://localhost:8280";
+	public static final String serverBase = prodServerBase;
 
 }
