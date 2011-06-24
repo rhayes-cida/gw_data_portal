2587ed5ab977330367b1e72ce8c4e3b808921692
I-Lin Kuo <ikuo@usgs.gov>
2011-06-24 20:36:20 +0000

    - Added qunit for testing

--- src/main/java/gov/usgs/DebugSettings.java
+++ src/main/java/gov/usgs/DebugSettings.java
@@ -1,10 +1,10 @@
 package gov.usgs;
 
 public class DebugSettings {
-	public static volatile boolean isTraceSet = false;
+	public static volatile boolean isTraceSet = true;
 
 	public static final String prodServerBase = "http://cida.usgs.gov";
 	public static final String localServerBase = "http://localhost:8280";
-	public static final String serverBase = prodServerBase;
+	public static final String serverBase = localServerBase;
 
 }
---
+++ src/main/webapp/assets/js/DocNavHelper.js
@@ -0,0 +1,8 @@
+DNH = {
+	extractText: function(doc, elementName){
+		if (!doc) return null;
+		var elmt = doc.getElementsByTagName(elementName)[0]
+	}
+		
+		
+}
