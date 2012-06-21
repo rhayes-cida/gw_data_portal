eef8264921e8ae9f00c9da4e704bbc10dc470978
Roger Hayes <rhayes@usgs.gov>
2012-06-21 20:08:54 +0000

    Clean up.

--- src/main/java/gov/usgs/ExportServlet.java
+++ src/main/java/gov/usgs/ExportServlet.java
@@ -9,12 +9,9 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -41,86 +38,17 @@ public class ExportServlet extends HttpServlet {
 		// Get response data.
 		RequestType serviceRequest = RequestType.download;
 		String urlString = serviceRequest.makeCacheUrl(agencyCd, siteNo);
-		System.out.println(urlString);
+		log("gov.usgs.ExportServlet.doGet url=" + urlString);
 		
 		// copy cache output to client
 		copyUrlContents(urlString, os);
 		os.flush();
+		os.close();
 	}
 	
-	protected void zipDoGet(HttpServletRequest req, HttpServletResponse resp)
-	throws ServletException, IOException {
-
-		String downloadToken = DOWNLOAD_TOKEN.parse(req);
-		String siteNo = SITE_NO.parse(req);
-		String agencyCd = AGENCY_CODE.parse(req);
-		String wlSnFlag = WATER_LEVEL_FLAG.parse(req);
-		String qwSnFlag = WATER_QUALITY_FLAG.parse(req);
-
-		resp.setContentType("application/zip");
-		resp.setHeader("Content-Disposition", "attachment; filename=gwdp_"
-				+ agencyCd + "_" + siteNo + ".zip");
-		resp.addCookie(new Cookie("exportToken", downloadToken));
-
-		ServletOutputStream os = resp.getOutputStream();
-
-		ZipOutputStream zos = new ZipOutputStream(os);
-
-		// fetch files
-		// Get response data.
-		RequestType serviceRequest = RequestType.well_log;
-		String urlString = serviceRequest.makeRESTUrl(agencyCd, siteNo);
-		zos.putNextEntry(new ZipEntry("well_log.xml"));
-		zipUrlContents(urlString, zos);
-		zos.flush();
-
-		if ("Yes".equals(wlSnFlag)) {
-			serviceRequest = RequestType.water_level;
-			urlString = serviceRequest.makeRESTUrl(agencyCd, siteNo);
-			zos.putNextEntry(new ZipEntry("water_level.xml"));
-			zipUrlContents(urlString, zos);
-			zos.flush();
-		}
-
-		if ("Yes".equals(qwSnFlag)) {
-			serviceRequest = RequestType.water_quality;
-			urlString = serviceRequest.makeRESTUrl(agencyCd, siteNo);
-			zos.putNextEntry(new ZipEntry("water_quality.xml"));
-			zipUrlContents(urlString, zos);
-			zos.flush();
-		}
-
-		zos.close();
-		os.flush();
-
-	}
-	
-	private void zipUrlContents(String urlString, ZipOutputStream zos) {
-
-		DataInputStream input = null;
-		StringBuffer page = new StringBuffer();
-
-		try {
-
-			URL url = new URL(urlString);
-			URLConnection conn = url.openConnection();
-			input = new DataInputStream(conn.getInputStream());
-			BufferedInputStream br = new BufferedInputStream(input);
-
-			String str = "";
-			byte[] buffer = new byte[1024];
-			int read;
-			while ((read = br.read(buffer)) != -1) {
-				zos.write(buffer, 0, read);
-			}
-
-			br.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	private void copyUrlContents(String urlString, OutputStream os) {
+	private void copyUrlContents(String urlString, OutputStream os) 
+			throws IOException, ServletException
+	{
 
 		try {
 
@@ -129,19 +57,27 @@ public class ExportServlet extends HttpServlet {
 			InputStream input = new DataInputStream(conn.getInputStream());
 			BufferedInputStream br = new BufferedInputStream(input);
 			try {
-				copy(br, os);
+				int ct = copy(br, os);
+				log("gov.usgs.ExportServlet.copyUrlContents done, ct=" + ct);
 			} finally {
 				br.close();
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
+		} 
+		catch (IOException ioe) {
+			throw ioe;
+		} 
+		catch (Exception e) {
+			log("problem in gov.usgs.ExportServlet.copyUrlContents", e);
+			throw new ServletException(e);
 		}
 	}
 
-	private void copy(InputStream is, OutputStream os) 
+	private int copy(InputStream is, OutputStream os) 
 			throws IOException
 	{
 		byte[] buf = new byte[1024];
+		
+		int tct = 0;
 		
 		while (true) {
 			int ct = is.read(buf);
@@ -149,7 +85,10 @@ public class ExportServlet extends HttpServlet {
 				break;
 			}
 			os.write(buf, 0, ct);
+			tct += ct;
 		}
+		
+		return tct;
 	}
 
 }
