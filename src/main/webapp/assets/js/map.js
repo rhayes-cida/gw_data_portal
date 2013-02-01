ff2a5630e8badb180084797492a4c1e5ea45484d
Roger Hayes <rhayes@usgs.gov>
2013-02-01 17:49:07 +0000

    RESOLVED - issue GWDP-521: Adjust Extent on OpenLayers Mapper http://internal.cida.usgs.gov/jira/browse/GWDP-521
    Restore mouse position control.

--- src/main/webapp/assets/js/map.js
+++ src/main/webapp/assets/js/map.js
@@ -38,18 +38,10 @@ GWDP.ui.initMap = function() {
             new OpenLayers.Control.PanZoom(),
             new OpenLayers.Control.OverviewMap({
             }),
-/*            
   			new OpenLayers.Control.MousePosition({
                 id: 'map-area-mouse-position',
-                numDigits: 6,
-            	// Defined formatOutput because the displayProjection was not working.
-            	formatOutput: function(lonLat){
-            		lonLat.transform(GWDP.ui.map.mercatorProjection, GWDP.ui.map.wgs84Projection);
-           			// return lonLat.toShortString();
-            		return lonLat.lon.toFixed(this.numDigits) + ", " + lonLat.lat.toFixed(this.numDigits);
-           		}
-           	}),
-*/            
+                numDigits: 6
+  			}),
             new OpenLayers.Control.ScaleLine({
                 id: 'map-area-scale-line'
             })
